4ef03da2c4abda12b60b9d1bdeb95ac34490e0db
rembelskiPiotr <rembelskipiotr92@gmail.com>
2021-01-20 15:22:51 +0100

    working version

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,45 @@
 class Rectangle:
 
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return ((self.width ** 2 + self.height ** 2) ** .5)
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        else:
+            return ("*" * self.width + "\n") * self.height
+
+    def get_amount_inside(self, shape):
+        return self.get_area() // shape.get_area()
+
+    def __str__(self):
+        return "Rectangle(width={}, height={})".format(self.width, self.height)
 
 
+class Square(Rectangle):
 
-class Square:
+    def __init__(self, side):
+        self.set_side(side)
+
+    def set_side(self, side):
+        Rectangle.set_width(self, side)
+        Rectangle.set_height(self, side)
+
+    def __str__(self):
+        return "Square(side={})".format(self.width)
